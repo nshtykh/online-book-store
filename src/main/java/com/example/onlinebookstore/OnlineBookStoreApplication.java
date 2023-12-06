5bfc2b494195f36525b860cc68f112074b62095e
Nataliia Shtykh <forstudying93@gmail.com>
2023-12-06 16:20:47 +0200

    removed CommandLineRunner bean

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,9 @@
 package com.example.onlinebookstore;
 
-import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.service.BookService;
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
@@ -16,19 +12,5 @@ public class OnlineBookStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book harryPotter1 = new Book();
-            harryPotter1.setTitle("Harry Potter and the Philosopher's Stone");
-            harryPotter1.setAuthor("Joanne Rowling");
-            harryPotter1.setIsbn("789451212");
-            harryPotter1.setPrice(BigDecimal.valueOf(250));
-
-            bookService.save(harryPotter1);
-            System.out.println(bookService.findAll());
-        };
     }
 }
