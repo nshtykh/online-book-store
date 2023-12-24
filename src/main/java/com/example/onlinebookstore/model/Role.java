6306b9322705869bf015921861ca881fb8142590
Nataliia Shtykh <forstudying93@gmail.com>
2023-12-24 21:37:15 +0200

    added jbbstypecode annotation

--- src/main/java/com/example/onlinebookstore/model/Role.java
+++ src/main/java/com/example/onlinebookstore/model/Role.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
@@ -16,7 +18,8 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(nullable = false, unique = true)
     private RoleName name;
 
     @Override
