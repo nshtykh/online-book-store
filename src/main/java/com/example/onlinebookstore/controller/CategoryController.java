e339b5236f28cadc3cb982c531a7e2daaf8af395
Nataliia Shtykh <forstudying93@gmail.com>
2023-12-30 17:07:32 +0200

    added crud methods to category controller

--- src/main/java/com/example/onlinebookstore/controller/CategoryController.java
+++ src/main/java/com/example/onlinebookstore/controller/CategoryController.java
@@ -1,11 +1,18 @@
 package com.example.onlinebookstore.controller;
 
+import com.example.onlinebookstore.dto.category.CategoryRequestDto;
 import com.example.onlinebookstore.dto.category.CategoryResponseDto;
 import com.example.onlinebookstore.service.category.CategoryService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +25,26 @@ public class CategoryController {
     @GetMapping
     public List<CategoryResponseDto> getAll(Pageable pageable) {
         return categoryService.getAll(pageable);
+    }
+
+    @PostMapping
+    public CategoryResponseDto createCategory(@RequestBody @Valid CategoryRequestDto requestDto) {
+        return categoryService.save(requestDto);
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponseDto getCategoryById(@PathVariable Long id) {
+        return categoryService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponseDto updateCategory(@PathVariable Long id,
+                                              @RequestBody @Valid CategoryRequestDto requestDto) {
+        return categoryService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteById(id);
     }
 }
--- src/main/java/com/example/onlinebookstore/service/category/CategoryServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/category/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.onlinebookstore.service.category;
 
+import com.example.onlinebookstore.dto.category.CategoryRequestDto;
 import com.example.onlinebookstore.dto.category.CategoryResponseDto;
+import com.example.onlinebookstore.exception.EntityNotFoundException;
 import com.example.onlinebookstore.mapper.CategoryMapper;
+import com.example.onlinebookstore.model.Category;
 import com.example.onlinebookstore.repository.CategoryRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+    private static final String EXCEPTION_MSG_CANNOT_FIND = "Can't find category with id: ";
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
 
@@ -20,5 +24,32 @@ public class CategoryServiceImpl implements CategoryService {
                 .stream()
                 .map(categoryMapper::toDto)
                 .toList();
+    }
+
+    @Override
+    public CategoryResponseDto save(CategoryRequestDto requestDto) {
+        Category category = categoryMapper.toModel(requestDto);
+        return categoryMapper.toDto(categoryRepository.save(category));
+    }
+
+    @Override
+    public CategoryResponseDto findById(Long id) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(EXCEPTION_MSG_CANNOT_FIND + id));
+        return categoryMapper.toDto(category);
+    }
+
+    @Override
+    public CategoryResponseDto update(Long id, CategoryRequestDto requestDto) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(EXCEPTION_MSG_CANNOT_FIND + id));
+        category.setName(requestDto.getName());
+        category.setDescription(requestDto.getDescription());
+        return categoryMapper.toDto(category);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
     }
 }
