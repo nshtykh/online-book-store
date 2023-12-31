eeb2306d90574a594a4b3ed9588c7dbb953de466
Nataliia Shtykh <forstudying93@gmail.com>
2023-12-31 16:05:36 +0200

    corrected request matchers in the security cinfig

--- src/main/java/com/example/onlinebookstore/config/SecurityConfig.java
+++ src/main/java/com/example/onlinebookstore/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/auth/", "/error",
-                                        "/swagger-ui/", "/v3/api-docs/**")
+                                .requestMatchers("/auth/**", "/error",
+                                        "/swagger-ui/**", "/v3/api-docs/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
