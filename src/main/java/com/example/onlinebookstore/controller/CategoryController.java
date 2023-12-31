15974d1c28248de99b03e3164465b3753fe271e7
Nataliia Shtykh <forstudying93@gmail.com>
2023-12-31 15:18:22 +0200

    added swagger to the category controller

--- src/main/java/com/example/onlinebookstore/controller/CategoryController.java
+++ src/main/java/com/example/onlinebookstore/controller/CategoryController.java
@@ -5,6 +5,8 @@ import com.example.onlinebookstore.dto.category.CategoryRequestDto;
 import com.example.onlinebookstore.dto.category.CategoryResponseDto;
 import com.example.onlinebookstore.service.book.BookService;
 import com.example.onlinebookstore.service.category.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,30 +24,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
+@Tag(name = "Category management", description = "Endpoints for managing categories")
 public class CategoryController {
     private final CategoryService categoryService;
     private final BookService bookService;
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping
+    @Operation(summary = "Get all categories", description = "Get a list of all the categories")
     public List<CategoryResponseDto> getAll(Pageable pageable) {
         return categoryService.getAll(pageable);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
+    @Operation(summary = "Save a new category", description = "Save a new category")
     public CategoryResponseDto createCategory(@RequestBody @Valid CategoryRequestDto requestDto) {
         return categoryService.save(requestDto);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping("/{id}")
+    @Operation(summary = "Get category by id", description = "Get category by id")
     public CategoryResponseDto getCategoryById(@PathVariable Long id) {
         return categoryService.findById(id);
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
+    @Operation(summary = "Update category by id", description = "Update category by id")
     public CategoryResponseDto updateCategory(@PathVariable Long id,
                                               @RequestBody @Valid CategoryRequestDto requestDto) {
         return categoryService.update(id, requestDto);
@@ -53,12 +60,15 @@ public class CategoryController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete category by id", description = "Delete category by id")
     public void deleteCategory(@PathVariable Long id) {
         categoryService.deleteById(id);
     }
 
     @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping("/{id}/books")
+    @Operation(summary = "Get all books by category id",
+            description = "Get a list of all the books by category id")
     public List<BookDtoWithoutCategoryIds> getBooksByCategoryId(
             @PathVariable(name = "id") Long categoryId) {
         return bookService.findAllByCategoryId(categoryId);
