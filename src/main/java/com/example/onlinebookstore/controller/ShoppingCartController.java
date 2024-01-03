98013624e65a5d2486f724ab28a115647c6b9304
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-03 22:38:03 +0200

    added swagger to the shopping cart controller

--- src/main/java/com/example/onlinebookstore/controller/ShoppingCartController.java
+++ src/main/java/com/example/onlinebookstore/controller/ShoppingCartController.java
@@ -5,6 +5,8 @@ import com.example.onlinebookstore.dto.cart.PutCartItemRequestDto;
 import com.example.onlinebookstore.dto.cart.ShoppingCartResponseDto;
 import com.example.onlinebookstore.model.User;
 import com.example.onlinebookstore.service.ShoppingCartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -20,10 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cart")
+@Tag(name = "Shopping cart management", description = "Endpoints for managing shopping cart")
 public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
 
     @PostMapping
+    @Operation(summary = "Add book to shopping cart", description = "Add book to shopping cart")
     public ShoppingCartResponseDto addToCart(
             @RequestBody @Valid AddToCartRequestDto requestDto,
             Authentication authentication) {
@@ -33,12 +37,14 @@ public class ShoppingCartController {
     }
 
     @GetMapping
+    @Operation(summary = "Get shopping cart", description = "Get shopping cart with books")
     public ShoppingCartResponseDto getShoppingCart(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return shoppingCartService.getCartByUserId(user.getId());
     }
 
     @DeleteMapping("/cart-items/{cartItemId}")
+    @Operation(summary = "Delete book from shopping cart", description = "Delete book from shopping cart")
     public ShoppingCartResponseDto removeFromCart(Authentication authentication,
                                                   @PathVariable Long cartItemId) {
         User user = (User) authentication.getPrincipal();
@@ -46,6 +52,7 @@ public class ShoppingCartController {
     }
 
     @PutMapping("/cart-items/{cartItemId}")
+    @Operation(summary = "Update book quantity in shopping cart", description = "Update book quantity in shopping cart")
     public ShoppingCartResponseDto updateCartItem(
             Authentication authentication,
             @PathVariable Long cartItemId,
