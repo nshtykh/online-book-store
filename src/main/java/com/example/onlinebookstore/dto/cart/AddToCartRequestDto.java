32e8dad5921358b9ff199a8c8bdf76338c072e6c
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-04 16:50:33 +0200

    correcte annotations in cart dto classes

--- src/main/java/com/example/onlinebookstore/dto/cart/AddToCartRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/cart/AddToCartRequestDto.java
@@ -1,12 +1,15 @@
 package com.example.onlinebookstore.dto.cart;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddToCartRequestDto {
+    @NotNull
     @Positive
     private Long bookId;
+    @NotNull
     @Positive
     private int quantity;
 }
