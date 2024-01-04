b59e00be12eb7dc27871cc5fb696152d68783479
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-05 01:43:50 +0200

    added swagger to order controller

--- src/main/java/com/example/onlinebookstore/controller/OrderController.java
+++ src/main/java/com/example/onlinebookstore/controller/OrderController.java
@@ -6,6 +6,8 @@ import com.example.onlinebookstore.dto.order.PatchOrderRequestDto;
 import com.example.onlinebookstore.dto.order.PostOrderRequestDto;
 import com.example.onlinebookstore.model.User;
 import com.example.onlinebookstore.service.order.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
+@Tag(name = "Order management", description = "Endpoints for managing orders")
 public class OrderController {
     private final OrderService orderService;
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping
+    @Operation(summary = "Get order history", description = "Get order history")
     public List<OrderResponseDto> getOrderHistory(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return orderService.getAllOrders(user.getId());
@@ -34,12 +38,14 @@ public class OrderController {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("{orderId}/items")
+    @Operation(summary = "Get order items", description = "Get order items by order id")
     public List<OrderItemResponseDto> getOrderItemsById(@PathVariable Long orderId) {
         return orderService.getOrderItemsByOrderId(orderId);
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("{orderId}/items/{itemId}")
+    @Operation(summary = "Get order item", description = "Get order item by order and item id")
     public OrderItemResponseDto getOrderItem(
             @PathVariable Long orderId,
             @PathVariable Long itemId) {
@@ -48,6 +54,7 @@ public class OrderController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PatchMapping("/{id}")
+    @Operation(summary = "Update order status", description = "Update order status")
     public OrderResponseDto updateOrderStatus(
             @PathVariable Long id,
             @Valid @RequestBody PatchOrderRequestDto requestDto) {
@@ -56,6 +63,7 @@ public class OrderController {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping
+    @Operation(summary = "Place a new order", description = "Place a new order and clear the cart")
     public OrderResponseDto placeOrder(
             Authentication authentication,
             @Valid @RequestBody PostOrderRequestDto requestDto) {
