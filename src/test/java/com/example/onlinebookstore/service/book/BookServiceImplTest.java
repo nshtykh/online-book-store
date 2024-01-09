d3e9deb18b73d3f25e807932aa918a16366bd615
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-09 14:13:16 +0200

    added tests for book  and category service

---
+++ src/test/java/com/example/onlinebookstore/service/book/BookServiceImplTest.java
@@ -0,0 +1,66 @@
+package com.example.onlinebookstore.service.book;
+
+import com.example.onlinebookstore.dto.book.BookResponseDto;
+import com.example.onlinebookstore.mapper.BookMapper;
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BookServiceImplTest {
+    @InjectMocks
+    private BookServiceImpl bookService;
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private BookMapper bookMapper;
+
+
+    @Test
+    public void findAll_ValidPageable_ReturnsAllBooks() {
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Harry Potter");
+        book.setAuthor("J. K. Rowling");
+        book.setIsbn("9781111111111");
+        book.setPrice(BigDecimal.valueOf(100));
+
+        BookResponseDto bookResponseDto = new BookResponseDto();
+        bookResponseDto.setId(book.getId());
+        bookResponseDto.setTitle(book.getTitle());
+        bookResponseDto.setAuthor(book.getAuthor());
+        bookResponseDto.setIsbn(book.getIsbn());
+        bookResponseDto.setPrice(book.getPrice());
+
+        Pageable pageable = PageRequest.of(0,10);
+        List<Book> books = List.of(book);
+        Page<Book> bookPage = new PageImpl<>(books, pageable, books.size());
+
+        List<BookResponseDto> expected = List.of(bookResponseDto);
+
+        Mockito.when(bookRepository.getAll(pageable)).thenReturn(bookPage);
+        Mockito.when(bookMapper.toDto(book)).thenReturn(bookResponseDto);
+
+        List<BookResponseDto> actual = bookService.findAll(pageable);
+
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected.get(0).getId(), actual.get(0).getId());
+        assertEquals(expected.get(0).getTitle(), actual.get(0).getTitle());
+    }
+}
---
+++ src/test/java/com/example/onlinebookstore/service/category/CategoryServiceImplTest.java
@@ -0,0 +1,56 @@
+package com.example.onlinebookstore.service.category;
+
+import com.example.onlinebookstore.dto.category.CategoryResponseDto;
+import com.example.onlinebookstore.mapper.CategoryMapper;
+import com.example.onlinebookstore.model.Category;
+import com.example.onlinebookstore.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceImplTest {
+    @InjectMocks
+    private CategoryServiceImpl categoryService;
+    @Mock
+    private CategoryRepository categoryRepository;
+    @Mock
+    private CategoryMapper categoryMapper;
+
+    @Test
+    void getAll_ValidPageable_ReturnsAllCategories() {
+        Category category = new Category();
+        category.setId(1L);
+        category.setName("fantasy");
+
+        CategoryResponseDto categoryDto = new CategoryResponseDto();
+        categoryDto.setId(category.getId());
+        categoryDto.setName(category.getName());
+
+        Pageable pageable = PageRequest.of(1,10);
+        List<Category> categories = List.of(category);
+        Page<Category> categoryPage = new PageImpl<>(categories, pageable, categories.size());
+
+        List<CategoryResponseDto> expected = List.of(categoryDto);
+
+        Mockito.when(categoryRepository.findAll(pageable)).thenReturn(categoryPage);
+        Mockito.when(categoryMapper.toDto(category)).thenReturn(categoryDto);
+
+        List<CategoryResponseDto> actual = categoryService.getAll(pageable);
+
+        assertEquals(expected.size(), actual.size());
+        assertEquals(expected.get(0).getId(), actual.get(0).getId());
+        assertEquals(expected.get(0).getName(), actual.get(0).getName());
+    }
+}
