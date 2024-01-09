ffe866d52e10630affb50d5cc96b2233f2cd8b74
Nataliia Shtykh <forstudying93@gmail.com>
2024-01-09 20:34:35 +0200

    added test for category controller

---
+++ src/test/java/com/example/onlinebookstore/controller/CategoryControllerTest.java
@@ -0,0 +1,76 @@
+package com.example.onlinebookstore.controller;
+
+import com.example.onlinebookstore.dto.category.CategoryResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CategoryControllerTest {
+    protected static MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeAll
+    static void beforeAll(
+            @Autowired WebApplicationContext applicationContext
+    ) {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @DisplayName("Get all categories")
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Sql(scripts = "classpath:database/"
+            + "categories/add-three-categories.sql",
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "classpath:database/"
+            + "categories/delete-all-categories.sql",
+            executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    void getAll_GivenThreeCategories_ShouldReturnAll() throws Exception {
+        List<CategoryResponseDto> expected = new ArrayList<>();
+        CategoryResponseDto fantasy = new CategoryResponseDto();
+        fantasy.setId(1L);
+        fantasy.setName("Fantasy");
+        CategoryResponseDto thriller = new CategoryResponseDto();
+        thriller.setId(2L);
+        thriller.setName("Thriller");
+        CategoryResponseDto adventure = new CategoryResponseDto();
+        adventure.setId(3L);
+        adventure.setName("Adventure");
+        expected.add(fantasy);
+        expected.add(thriller);
+        expected.add(adventure);
+
+        MvcResult result = mockMvc.perform(get("/categories")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        CategoryResponseDto[] actual = objectMapper.readValue(result.getResponse().getContentAsByteArray(), CategoryResponseDto[].class);
+        Assertions.assertEquals(expected.size(), actual.length);
+        Assertions.assertEquals(expected, Arrays.stream(actual).toList());
+    }
+}
---
+++ src/test/resources/database/categories/add-three-categories.sql
@@ -0,0 +1,3 @@
+insert categories (id, name) values (1, 'Fantasy');
+insert categories (id, name) values (2, 'Thriller');
+insert categories (id, name) values (3, 'Adventure');
